e4a0b4782f3f405db9247065b90ebdbd489eb82e
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2022-08-19 23:57:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Pertamaku"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 100,
+                child: Text(
+                  "Aku sedang melatih kemampuan Flutter saya",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20),
+                ))),
+      ),
+    );
+  }
+}
